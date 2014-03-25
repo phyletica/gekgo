b7389c911bacb5b5b01bd8cc3d3c0bb812949ac4
Jamie Oaks <joaks1@gmail.com>
2014-03-24 23:09:43 -0700

    Adding scripts for qc of demultiplexed msg seqs.

---
+++ bin/qc_msg_plate2_demultiplexed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ./project_paths.sh
+
+seq_dir=$MSG_PLATE2_DIR/demultiplexed-raw/fastq
+out_dir=$seq_dir/qc-output
+
+if [ ! -d "$out_dir" ]
+then
+    mkdir "$out_dir"
+fi
+
+for gz_file in "$seq_dir"/*.fastq.gz
+do
+    fq_file=${gz_file/\.gz/}
+    gzip -d "$gz_file"
+    IlluQC.pl -se "$fq_file" N A -p 2 -onlyStat -t 2 -o "$out_dir"
+    gzip "$fq_file"
+done
+
