82d785c75c85f8763067c23b3f7ab04c396b92c6
Jamie R. Oaks <joaks1@gmail.com>
2017-08-24 10:19:50 -0500

    Clearing out old pyrad assembly of MSG reads.
    Removing all of the old pyrad code and output for demultiplexing and
    assembling the reads from the three MSG plates.
    
    Making way for revamping denovo assemblies of MSG reads using the
    updated ipyrad package.

--- bin/seqsummary-demultiplexed-raw-plate1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ./project_paths.sh
-
-seq_dir="$MSG_PLATE1_DIR"/demultiplexed-raw/fastq
-
-seqsummary.py "$seq_dir"/*.fq.gz > "$seq_dir"/seqsummary-output.txt
-
--- bin/seqsummary-demultiplexed-raw-plate3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ./project_paths.sh
-
-seq_dir="$MSG_PLATE3_DIR"/demultiplexed-raw/fastq
-
-seqsummary.py "$seq_dir"/*.fq.gz > "$seq_dir"/seqsummary-output.txt
-
