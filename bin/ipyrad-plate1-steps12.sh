48eb475555aa65af2e0082f0e720dd37362829c2
Jamie R. Oaks <joaks1@gmail.com>
2017-08-24 10:23:52 -0500

    Adding ipyrad parameter files.
    Adding ipyrad parameter files for each of the 3 MSG plates. Also, adding
    shell scripts to run the first 2 steps of the ipyrad assemply
    (demultiplexing and filtering) for each plate of reads.

---
+++ bin/ipyrad-plate1-steps12.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=1:00:00
+#PBS -j oe
+#PBS -l jobflags=ADVRES:jro0014_lab.56281
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    condaenv on
+    condaenv
+fi
+
+ipyrad -p params-plate1.txt -s 12 -c 20 1>ipyrad-plate1-steps12.sh.out 2>&1
