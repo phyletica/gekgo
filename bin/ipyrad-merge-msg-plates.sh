6bedd8ae6c9b916e519ce240475295cb5b42fa4d
Jamie R. Oaks <joaks1@gmail.com>
2017-08-24 10:27:29 -0500

    Adding script to merge MSG assemblies.
    Adding shell script to use ipyrad to merge the assemblies of the reads
    of the 3 MSG plates. Previously steps 1 and 2 (demultiplexing and
    filtering) were run on each plate. This script merges the demultiplexed
    and filtered reads from all three MSG plates into a single assembly, so
    that the remaining steps can be executed across all the samples from the
    3 plates.
    
    Also, adding the stdout/stderr from running the added script.

---
+++ bin/ipyrad-merge-msg-plates.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=1:00:00
+#PBS -j oe
+#PBS -l jobflags=ADVRES:jro0014_lab.56281
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    condaenv on
+    condaenv
+fi
+
+ipyrad -m plates123 params-plate1.txt params-plate2.txt params-plate3.txt 1>ipyrad-merge-msg-plates.sh.out 2>&1
