e4248f6f6a069df2de692a93df0352baf22813d7
Jamie R. Oaks <joaks1@gmail.com>
2017-08-30 11:11:04 -0500

    Debugging sript to convert ipyrad assemblies.
    Also, adding stderr reported by the script.

--- bin/convert-ipyrad-assemblies-to-nexus.sh
+++ bin/convert-ipyrad-assemblies-to-nexus.sh
@@ -3,19 +3,19 @@
 scriptname="$(basename "$0")"
 stderrout="${scriptname}.stderr.out"
 
-source "./project_paths.sh"
+source ./project_paths.sh
 
-if [ ! -d "$MSG_ALIGNMENT_DIR" ]
+if [ ! -d "$MSG_ALIGNMENTS_DIR" ]
 then
-    mkdir -p "$MSG_ALIGNMENT_DIR"
+    mkdir -p "$MSG_ALIGNMENTS_DIR"
 fi
 
-for loci_path in "${MSG_IPYRAD_DIR}/*_outfiles/*.loci
+for loci_path in ${MSG_IPYRAD_DIR}/*_outfiles/*.loci
 do
     loci_file_name="$(basename "$loci_path")"
     prefix="${loci_file_name%.*}"
-    nex_path = "${MSG_ALIGNMENT_DIR}/${prefix}.nex"
-    biallelic_nex_path = "${MSG_ALIGNMENT_DIR}/${prefix}-polyallelic-sites-removed.nex"
+    nex_path="${MSG_ALIGNMENTS_DIR}/${prefix}.nex"
+    biallelic_nex_path="${MSG_ALIGNMENTS_DIR}/${prefix}-polyallelic-sites-removed.nex"
 
-    loci2nex "$loci_path" 1>"$nex_path" 2>"$stderrout"
+    loci2nex "$loci_path" 1>"$nex_path" 2>>"$stderrout"
 done
