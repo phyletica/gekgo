4aa5651f7fb6bacb39f45c55d02308e4e0408f19
Jamie R. Oaks <joaks1@gmail.com>
2017-11-28 14:56:38 -0600

    Adding files for "conc5" ecoevolity analyses.

---
+++ bin/ecoevolity-scripts/cyrtodactylus-conc5-rate200-run-1.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+#PBS -q gen28
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=20:00:00
+#PBS -j oe
+#PBS -W group_list=jro0014_lab
+#PBS -W x=FLAGS:ADVRES:jro0014_s28.162459
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source "${PBS_O_HOME}/.bash_profile"
+    cd "$PBS_O_WORKDIR"
+    module load gcc/5.3.0
+fi
+
+prefix=../../data/genomes/msg/ecoevolity-output/run-1-
+
+ecoevolity --seed 11973529 --prefix ../../data/genomes/msg/ecoevolity-output/run-1- --nthreads 4 --relax-missing-sites --relax-constant-sites --relax-triallelic-sites ../../data/genomes/msg/ecoevolity-configs/cyrtodactylus-conc5-rate200.yml 1>../../data/genomes/msg/ecoevolity-output/run-1-cyrtodactylus-conc5-rate200.out 2>&1
