fa3fd9a1c71225705e0f811062fc2281fa2538c8
Jamie R. Oaks <joaks1@gmail.com>
2018-11-17 03:38:03 -0600

    Adding simcoevolity scripts.
    Adding shell scripts to generate simulated multi-locus datasets that
    match the empirical dataset (i.e., same number and length of loci, and
    the same number of missing cells for each locus).

---
+++ bin/simcoevolity-scripts/simcoevolity-gekko-rate2000.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+simname="gekko-rate2000"
+cfgpath="../../data/genomes/msg/ecoevolity-configs/${simname}.yml"
+outputdir="../../data/genomes/msg/ecoevolity-simulations/${simname}/batch001"
+rngseed=285034733
+nreps=500
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -n "$nreps" -o "$outputdir" --relax-missing-sites --relax-constant-sites --relax-triallelic-sites --charsets "$cfgpath"
