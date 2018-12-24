c8b8f8ca72887846d292a9b1ef54b7ec4e3c2d89
Jamie R. Oaks <joaks1@gmail.com>
2018-12-23 22:46:09 -0600

    Adding more simcoevolity scripts.

---
+++ bin/simcoevolity-scripts/simcoevolity-gekko-rate2000-batch003.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+username="$USER"
+if [ "$username" == "aubjro" ]
+then
+    module load gcc/6.1.0
+fi
+
+if [ -n "$PBS_JOBNAME" ]
+then
+    source ${PBS_O_HOME}/.bash_profile
+    cd $PBS_O_WORKDIR
+
+    module load gcc/5.3.0
+fi
+
+simname="gekko-conc5-rate2000"
+cfgpath="../../data/genomes/msg/ecoevolity-configs/${simname}.yml"
+outputdir="../../data/genomes/msg/ecoevolity-simulations/${simname}/batch003"
+rngseed=129291392
+nreps=100
+
+mkdir -p "$outputdir"
+
+simcoevolity --seed="$rngseed" -n "$nreps" -o "$outputdir" --relax-missing-sites --relax-constant-sites --relax-triallelic-sites --charsets "$cfgpath"
