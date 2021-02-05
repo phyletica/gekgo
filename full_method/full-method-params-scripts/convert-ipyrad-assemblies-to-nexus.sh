285ffbd5c222d1180a4b06b09c6a70ac494bf3b6
Jamie R. Oaks <joaks1@gmail.com>
2021-02-05 13:23:20 -0600

    Updating script for converting ipyrad to nex.
    Updating 'convert-ipyrad-assemblies-to-nexus.sh'.
    Adding conversion of Gekko loci file.

--- full_method/full-method-params-scripts/convert-ipyrad-assemblies-to-nexus.sh
+++ full_method/full-method-params-scripts/convert-ipyrad-assemblies-to-nexus.sh
@@ -13,12 +13,14 @@ then
     mkdir -p "$alignment_dir"
 fi
 
-cyrt_loci_paths=( \
+loci_paths=( \
     Cyrtodactylus-reduced3_n80.loci.gz \
+    Gekko-reduced2_n81.loci.gz
 )
 cyrt_label_csv_path="cyrtodactylus-id-changes.csv"
+gekko_label_csv_path="gekko-id-changes.csv"
 
-for loci_path in ${cyrt_loci_paths[@]}
+for loci_path in ${loci_paths[@]}
 do
     loci_file_name="$(basename "$loci_path")"
     prefix="${loci_file_name%.*}"
@@ -26,11 +28,24 @@ do
     nex_path="${alignment_dir}/${prefix}.nex"
     biallelic_nex_path="${alignment_dir}/${prefix}-polyallelic-sites-removed.nex"
 
+    csv_path=""
+    if [[ "$loci_path" == [Cc]yrt* ]]
+    then
+        csv_path="$cyrt_label_csv_path"
+    elif [[ "$loci_path" == [Gg]ekko* ]]
+    then
+        csv_path="$gekko_label_csv_path"
+    else
+        echo "ERROR: Unexpected loci_path \"$loci_path\""
+        exit 1
+    fi
+
+
     loci2nex --charsets \
-        -m "$cyrt_label_csv_path" \
+        -m "$csv_path" \
         "$loci_path" 1>"$nex_path" 2>>"$stderrout"
     loci2nex --charsets \
         --remove-triallelic-sites \
-        -m "$cyrt_label_csv_path" \
+        -m "$csv_path" \
         "$loci_path" 1>"$biallelic_nex_path" 2>>"$stderrout"
 done
