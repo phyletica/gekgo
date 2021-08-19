e0a08516a9e5bec9786327eec40c95d88d73819a
Jamie R. Oaks <joaks1@gmail.com>
2021-08-19 14:19:36 -0500

    Adding small cyrt phycoeval dataset.
    Updating 'convert-ipyrad-assemblies-to-nexus.sh' to create small
    Cyrtodactylus data set for use in tutorials.

--- phycoeval-msg-assemblies/ipyrad-assemblies/convert-ipyrad-assemblies-to-nexus.sh
+++ phycoeval-msg-assemblies/ipyrad-assemblies/convert-ipyrad-assemblies-to-nexus.sh
@@ -6,6 +6,11 @@
 scriptname="$(basename "$0")"
 stderrout="${scriptname}.stderr.out"
 
+if [ -e "$stderrout" ]
+then
+    rm "$stderrout"
+fi
+
 alignment_dir="nexus-alignments"
 
 if [ ! -d "$alignment_dir" ]
@@ -51,3 +56,32 @@ do
         -m "$csv_path" \
         "$loci_path" 1>"$biallelic_nex_path" 2>>"$stderrout"
 done
+
+
+# Create small dataset for phycoeval tutorial
+nex_path="${alignment_dir}/Cyrtodactylus-tutorial-data.nex"
+loci2nex --charsets \
+    --seed 123 \
+    -m "$cyrt_label_csv_path" \
+    --subsample 50 \
+    -d CDS_2844_Cyrtodactylus_sumuroi_Samar \
+    -d CDS_3932_Cyrtodactylus_philippinicus_Lubang \
+    -d CDS_4485_Cyrtodactylus_annulatus_Bohol \
+    -d CDS_5823_Cyrtodactylus_philippinicus_Luzon \
+    -d CWL_89_Cyrtodactylus_annulatus_Cebu \
+    -d RMB_11670_Cyrtodactylus_jambangan_Mindanao \
+    -d RMB_12836_Cyrtodactylus_philippinicus_Luzon \
+    -d RMB_13366_Cyrtodactylus_philippinicus_Luzon \
+    -d RMB_13669_Cyrtodactylus_philippinicus_Luzon \
+    -d RMB_13964_Cyrtodactylus_philippinicus_Luzon \
+    -d RMB_14453_Cyrtodactylus_philippinicus_Luzon \
+    -d RMB_15119_Cyrtodactylus_philippinicus_Luzon \
+    -d RMB_5754_Cyrtodactylus_philippinicus_CamiguinNorte \
+    -d RMB_5905_Cyrtodactylus_philippinicus_BabuyanClaro \
+    -d RMB_6259_Cyrtodactylus_philippinicus_Polillo \
+    -d RMB_7647_Cyrtodactylus_tautbatorum_Palawan \
+    -d RMB_7859_Cyrtodactylus_redimiculus_Palawan \
+    -d RMB_8043_Cyrtodactylus_annulatus_CamiguinSur \
+    -d RMB_8444_Cyrtodactylus_mamanwa_Dinagat \
+    -d RMB_8933_Cyrtodactylus_gubaot_Leyte \
+    "Cyrtodactylus-reduced5_n27.loci.gz" 1>"$nex_path"
